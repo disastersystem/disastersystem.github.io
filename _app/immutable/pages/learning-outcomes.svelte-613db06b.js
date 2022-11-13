79d9fb0b6802c91f5a9062a8d496f52e3ff5015a
robinvb <robin.vigdal.bekkevold@gmail.com>
2022-11-14 00:31:14 +0100

    Update som updated information.

---
+++ _app/immutable/pages/learning-outcomes.svelte-613db06b.js
@@ -0,0 +1,33 @@
+import{S as ce,i as de,s as me,e as o,k as d,w as ue,t as u,N as pe,c as i,d as t,m,a as r,x as he,h as p,b as f,f as R,J as e,g as se,y as ge,E as fe,q as ve,o as ye,B as _e}from"../chunks/index-93a2a006.js";import{B as be}from"../chunks/Breadcrumbs-5a2abd8b.js";function qe(le){let h,H,v,_,n,y,C,b,D,O,q,T,A,a,E,B,P,I,j,M,L,N,V,w,z,J,x,U,F,$,G,K,g,Q,S;return y=new be({props:{pages:[["/","Home"],["/learning-outcomes","Learning outcomes"]]}}),{c(){h=o("meta"),H=d(),v=o("main"),_=o("div"),n=o("div"),ue(y.$$.fragment),C=d(),b=o("h1"),D=u("Learning outcomes"),O=d(),q=o("h3"),T=u("At the end of the COSI Programme, the student should be able to..."),A=d(),a=o("ul"),E=o("li"),B=u(`describe and apply fundamental aspects of\r
+                    computational colour science and data science, optics\r
+                    and photonics, measurement systems and spectral\r
+                    technologies, digital imaging and computer vision,\r
+                    mathematics and computation;`),P=d(),I=o("li"),j=u(`implement and apply current colour and spectral\r
+                    technologies, and other High-Tech devices used in\r
+                    digital imaging, demonstrating the ability to design and\r
+                    implement colour data processing algorithms;`),M=d(),L=o("li"),N=u(`calculate a range of colour quantities and image quality\r
+                    quantities, apply transforms, derive and implement\r
+                    colour and image metrics;`),V=d(),w=o("li"),z=u(`analyse colour technology and machine vision problems\r
+                    into components, construct solutions and design\r
+                    processing workflows;`),J=d(),x=o("li"),U=u(`model colour and digital image processes and predict a\r
+                    range of quantities including visual and psychometric\r
+                    response, colour appearance, perception of quality,\r
+                    spectral and colorimetric reproduction, 2D and 3D spatial\r
+                    models, display functions;`),F=d(),$=o("li"),G=u(`evaluate own and previous work from both theoretical,\r
+                    practical and application perspective`),K=d(),g=o("p"),Q=u("The definition of above learning qualities are variable as they are the result of the close involvement of our industrial partners in the learning design of COSI. Hence they are likely to change from one semester to another to best meet the requirements of employers."),this.h()},l(s){const k=pe('[data-svelte="svelte-jyz56"]',document.head);h=i(k,"META",{name:!0,content:!0}),k.forEach(t),H=m(s),v=i(s,"MAIN",{class:!0});var W=r(v);_=i(W,"DIV",{class:!0});var X=r(_);n=i(X,"DIV",{style:!0});var c=r(n);he(y.$$.fragment,c),C=m(c),b=i(c,"H1",{class:!0});var Y=r(b);D=p(Y,"Learning outcomes"),Y.forEach(t),O=m(c),q=i(c,"H3",{class:!0});var Z=r(q);T=p(Z,"At the end of the COSI Programme, the student should be able to..."),Z.forEach(t),A=m(c),a=i(c,"UL",{class:!0});var l=r(a);E=i(l,"LI",{});var ee=r(E);B=p(ee,`describe and apply fundamental aspects of\r
+                    computational colour science and data science, optics\r
+                    and photonics, measurement systems and spectral\r
+                    technologies, digital imaging and computer vision,\r
+                    mathematics and computation;`),ee.forEach(t),P=m(l),I=i(l,"LI",{});var te=r(I);j=p(te,`implement and apply current colour and spectral\r
+                    technologies, and other High-Tech devices used in\r
+                    digital imaging, demonstrating the ability to design and\r
+                    implement colour data processing algorithms;`),te.forEach(t),M=m(l),L=i(l,"LI",{});var ae=r(L);N=p(ae,`calculate a range of colour quantities and image quality\r
+                    quantities, apply transforms, derive and implement\r
+                    colour and image metrics;`),ae.forEach(t),V=m(l),w=i(l,"LI",{});var ne=r(w);z=p(ne,`analyse colour technology and machine vision problems\r
+                    into components, construct solutions and design\r
+                    processing workflows;`),ne.forEach(t),J=m(l),x=i(l,"LI",{});var oe=r(x);U=p(oe,`model colour and digital image processes and predict a\r
+                    range of quantities including visual and psychometric\r
+                    response, colour appearance, perception of quality,\r
+                    spectral and colorimetric reproduction, 2D and 3D spatial\r
+                    models, display functions;`),oe.forEach(t),F=m(l),$=i(l,"LI",{});var ie=r($);G=p(ie,`evaluate own and previous work from both theoretical,\r
+                    practical and application perspective`),ie.forEach(t),l.forEach(t),K=m(c),g=i(c,"P",{class:!0,style:!0});var re=r(g);Q=p(re,"The definition of above learning qualities are variable as they are the result of the close involvement of our industrial partners in the learning design of COSI. Hence they are likely to change from one semester to another to best meet the requirements of employers."),re.forEach(t),c.forEach(t),X.forEach(t),W.forEach(t),this.h()},h(){document.title="Learning outcomes - COSI",f(h,"name","description"),f(h,"content",""),f(b,"class","cosi-title-1"),f(q,"class","cosi-title-2 mt-16 mb-4"),f(a,"class","cosi-ul"),f(g,"class","cosi-p mt-20 pt-4 italic"),R(g,"color","#494949"),R(g,"border-top","1px solid #ddd"),R(n,"max-width","766px"),f(_,"class","flex justify-center mt-40 mb-20"),f(v,"class","px-6")},m(s,k){e(document.head,h),se(s,H,k),se(s,v,k),e(v,_),e(_,n),ge(y,n,null),e(n,C),e(n,b),e(b,D),e(n,O),e(n,q),e(q,T),e(n,A),e(n,a),e(a,E),e(E,B),e(a,P),e(a,I),e(I,j),e(a,M),e(a,L),e(L,N),e(a,V),e(a,w),e(w,z),e(a,J),e(a,x),e(x,U),e(a,F),e(a,$),e($,G),e(n,K),e(n,g),e(g,Q),S=!0},p:fe,i(s){S||(ve(y.$$.fragment,s),S=!0)},o(s){ye(y.$$.fragment,s),S=!1},d(s){t(h),s&&t(H),s&&t(v),_e(y)}}}class Le extends ce{constructor(h){super(),de(this,h,null,qe,me,{})}}export{Le as default};
